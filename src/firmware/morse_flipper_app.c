5117ef852e0afe60a536a63feabb23a1f57d42e2
Richard <yo3gnd.dev.493@yo3gnd.ro>
2026-03-08 10:23:08 +0200

    leave fap file as entrypoint only

--- src/firmware/morse_flipper_app.c
+++ src/firmware/morse_flipper_app.c
@@ -284,14 +284,3 @@ void morse_flipper_shutdown(MorseFlipperApp* app)
     furi_record_close(RECORD_NOTIFICATION);
     furi_record_close(RECORD_GUI);
 }
-
-int32_t morse_flipper_fap(void* p)
-{
-    MorseFlipperApp* app;
-    UNUSED(p);
-
-    app = morse_flipper_boot();
-    view_dispatcher_run(morse_flipper_view_dispatcher_get(app));
-    morse_flipper_shutdown(app);
-    return 0;
-}
